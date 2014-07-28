0e5977a9289047c436598f537d808d01b077f930
Evan Hahn <me@evanhahn.com>
2014-07-28 11:52:25 -0700

    Add browser support

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-module.exports = clamp
-
 function clamp(value, min, max) {
   return min < max
     ? (value < min ? min : value > max ? max : value)
     : (value < max ? max : value > min ? min : value)
 }
+
+if (typeof module !== 'undefined')
+  module.exports = clamp
